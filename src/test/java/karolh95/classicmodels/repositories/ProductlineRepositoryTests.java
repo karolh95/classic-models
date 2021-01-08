c8710a67c83040f5b2b03ffb1ebc67953c21d718
karolh95 <karolh95@live.com>
2021-01-08 11:18:28 +0100

    The Productline Repository Tests now uses Productline Factory.

--- src/test/java/karolh95/classicmodels/repositories/ProductlineRepositoryTests.java
+++ src/test/java/karolh95/classicmodels/repositories/ProductlineRepositoryTests.java
@@ -1,6 +1,7 @@
 package karolh95.classicmodels.repositories;
 
 import karolh95.classicmodels.models.Productline;
+import karolh95.classicmodels.utils.ProductlineFactory;
 import org.junit.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -18,16 +19,7 @@ public class ProductlineRepositoryTests {
 
     @Test
     public void shouldSaveProductline(){
-        String PRODUCTLINE = "Classic Cars";
-        String TEXT = "Attention car enthusiasts: Make your wildest car ownership dreams come true. Whether you are looking for classic muscle cars, dream sports cars or movie-inspired miniatures, you will find great choices in this category. These replicas feature superb attention to detail and craftsmanship and offer features such as working steering system, opening forward compartment, opening rear trunk with removable spare wheel, 4-wheel independent spring suspension, and so on. The models range in size from 1:10 to 1:24 scale and include numerous limited edition and several out-of-production vehicles. All models include a certificate of authenticity from their manufacturers and come fully assembled and ready for display in the home or office.";
-        String HTML = null;
-        byte[] IMAGE = new byte[0];
-
-        Productline productline = new Productline();
-        productline.setProductLine(PRODUCTLINE);
-        productline.setTextDescription(TEXT);
-        productline.setHtmlDescription(HTML);
-        productline.setImage(IMAGE);
+        Productline productline = ProductlineFactory.getProductline();
 
         productline = repository.save(productline);
 
