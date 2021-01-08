6722fe164ac13de32cf48e07bf0e2a6711d73803
karolh95 <karolh95@live.com>
2021-01-08 17:48:04 +0100

    Fixed: The ProductlineService Tests used a autowired repository instead of a mock.

--- src/test/java/karolh95/classicmodels/services/ProductlineServiceTests.java
+++ src/test/java/karolh95/classicmodels/services/ProductlineServiceTests.java
@@ -11,55 +11,94 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.doReturn;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class ProductlineServiceTests {
 
     @Autowired
-    private ProductlineRepository productlineRepository;
-
-    @Autowired
     private ProductlineService productlineService;
+
+    @MockBean
+    private ProductlineRepository productlineRepository;
 
     private ProductlineDTO dto;
 
     @Before
     public void setUp() {
         dto = ProductlineFactory.getPoductlineDto();
-        productlineRepository.deleteAll();
     }
 
     @Test
     public void shouldSaveProductline() {
 
+        Productline productline = ProductlineFactory.getProductline();
+        doReturn(false)
+                .when(productlineRepository)
+                .existsById(anyString());
+        doReturn(productline)
+                .when(productlineRepository)
+                .save(any(Productline.class));
+
         dto = productlineService.saveProductline(dto);
 
         assertNotNull(dto);
+        assertEquals(productline.getProductLine(), dto.getProductLine());
+        assertEquals(productline.getTextDescription(), dto.getTextDescription());
+        assertEquals(productline.getHtmlDescription(), dto.getHtmlDescription());
+        assertArrayEquals(productline.getImage(), dto.getImage());
     }
 
     @Test
     public void shouldUpdateProductline() {
 
-        final String newHTMLdescription = "new HTML description";
+        final String newTEXT = "new text description";
+        final String newHTML = "new HTML description";
+        final byte[] newIMAGE = new byte[5];
 
-        dto = productlineService.saveProductline(dto);
+        Productline productline = ProductlineFactory.getProductline();
+        productline.setTextDescription(newTEXT);
+        productline.setHtmlDescription(newHTML);
+        productline.setImage(newIMAGE);
 
-        dto.setHtmlDescription(newHTMLdescription);
+        doReturn(Optional.of(ProductlineFactory.getProductline()))
+                .when(productlineRepository)
+                .findById(anyString());
+        doReturn(productline)
+                .when(productlineRepository)
+                .save(any(Productline.class));
 
-        dto = productlineService.updateProductline(dto);
+        dto.setTextDescription(newTEXT);
+        dto.setHtmlDescription(newHTML);
+        dto.setImage(newIMAGE);
+
+        ProductlineDTO result = productlineService.updateProductline(dto);
 
         assertNotNull(dto);
-        assertEquals(newHTMLdescription, dto.getHtmlDescription());
+        assertEquals(dto.getTextDescription(), result.getTextDescription());
+        assertEquals(dto.getHtmlDescription(), result.getHtmlDescription());
+        assertArrayEquals(dto.getImage(), result.getImage());
     }
 
     @Test
     public void shouldNotUpdateProductline_notExists() {
+
+        doReturn(Optional.empty())
+                .when(productlineRepository)
+                .findById(anyString());
 
         assertThrows(
                 ProductlineNotFoundException.class,
@@ -70,9 +109,9 @@ public class ProductlineServiceTests {
     @Test
     public void shouldNotSaveProductline_alreadyExists() {
 
-        dto = productlineService.saveProductline(dto);
-
-        assertNotNull(dto);
+        doReturn(true)
+                .when(productlineRepository)
+                .existsById(anyString());
 
         assertThrows(
                 ProductlineAlreadyExists.class,
@@ -83,13 +122,25 @@ public class ProductlineServiceTests {
     @Test
     public void shouldFindProductline() {
 
-        dto = productlineService.saveProductline(dto);
+        doReturn(Optional.of(ProductlineFactory.getProductline()))
+                .when(productlineRepository)
+                .findById(anyString());
 
-        assertNotNull(dto);
-
-        ProductlineDTO productlineDto = productlineService.findProductlineByProductLine(dto.getProductLine());
-
-        assertNotNull(productlineDto);
+        assertDoesNotThrow(
+                () -> productlineService.findProductlineByProductLine(dto.getProductLine())
+        );
     }
 
+    @Test
+    public void shouldNotFindProductline() {
+
+        doReturn(Optional.empty())
+                .when(productlineRepository)
+                .findById(anyString());
+
+        assertThrows(
+                ProductlineNotFoundException.class,
+                () -> productlineService.findProductlineByProductLine("productLine")
+        );
+    }
 }
